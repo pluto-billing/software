0aa8f7a3547e8be49de572108e828b30646398c3
GabeSystems <straxxmusicteam@gmail.com>
2019-08-23 00:53:06 -0400

    small changes and fix formatting

--- setup/index.php
+++ setup/index.php
@@ -35,58 +35,57 @@
 ?>
 
 <html>
-    <head>
-        <title>Pluto Billing - Software Setup</title>
-
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/uikit.min.css" />
-      	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-      	<link rel="stylesheet" href="css/style.css" />
-        <link rel="stylesheet" href="css/notyf.min.css" />
-        <script src="js/uikit.min.js"></script>
-      	<script src="js/uikit-icons.min.js"></script>
-    </head>
-    <body>
-        <div uk-sticky="media: 960" class="uk-navbar-container tm-navbar-container uk-sticky uk-active" style="position: fixed; top: 0px; width: 1903px;">
-            <div class="uk-container uk-container-expand">
-                <nav uk-navbar>
-                    <div class="uk-navbar-left">
-                        <a href="" class="uk-navbar-item uk-logo">
-                            Pluto Billing - Software Setup
-                        </a>
-                    </div>
-                </nav>
-            </div>
-        </div>
-        <div class="content-background">
-            <div class="uk-section-large">
-                <div class="uk-container uk-container-large">
-                    <div uk-grid class="uk-child-width-1-1@s uk-child-width-2-3@l">
-                        <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
-                        <div class="uk-width-1-1@s uk-width-3-5@l uk-width-1-3@xl">
-                            <div class="uk-card uk-card-default">
-                              <div class="uk-alert-warning" uk-alert>
-                                  <a class="uk-alert-close" uk-close></a>
-                                  <p><span class="uk-text-bold">Development Notice:</span> Please be aware that you are viewing a pre-alpha version of Pluto and things are subject to change.</p>
-                                </div>
-                                <div class="uk-card-header">
-                                  Ready to Setup Pluto?
-                                </div>
-                                <div class="uk-card-body">
-                                    <center>
-                                        <h2>Pluto Software Setup</h2>
-                                        <p>Before we begin our Pluto journey we must prepare. This setup is required before you blast off!</p>
-                                        <a href="setup.php" class="uk-button uk-button-primary">Begin Setup</a>
-                                        <a href="https://pluto.crypticnode.host/docs" class="uk-button uk-button-secondary" target="_blank">Documentation</a>
-                                    </center>
-                                  </div>
-                            </div>
-                        </div>
-                        <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
-                    </div>
+  <head>
+    <title>Pluto Billing - Software Setup</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/notyf.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+  </head>
+  <body>
+    <div uk-sticky="media: 960" class="uk-navbar-container tm-navbar-container uk-sticky uk-active" style="position: fixed; top: 0px; width: 1903px;">
+      <div class="uk-container uk-container-expand">
+        <nav uk-navbar>
+          <div class="uk-navbar-left">
+            <a href="" class="uk-navbar-item uk-logo">
+            Pluto Billing - Software Setup
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="content-background">
+      <div class="uk-section-large">
+        <div class="uk-container uk-container-large">
+          <div uk-grid class="uk-child-width-1-1@s uk-child-width-2-3@l">
+            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
+            <div class="uk-width-1-1@s uk-width-3-5@l uk-width-1-3@xl">
+              <div class="uk-card uk-card-default">
+                <div class="uk-alert-warning" uk-alert>
+                  <a class="uk-alert-close" uk-close></a>
+                  <p><span class="uk-text-bold">Development Notice:</span> Please be aware that you are viewing a pre-alpha version of Pluto and things are subject to change.</p>
                 </div>
+                <div class="uk-card-header">
+                  <span class="uk-text-bold">Step One:</span> Begin Installation
+                </div>
+                <div class="uk-card-body">
+                  <center>
+                    <h2>Pluto Software Setup</h2>
+                    <p>Before we begin our Pluto journey we must prepare. This setup is required before you blast off!</p>
+                    <a href="https://pluto.crypticnode.host/docs" class="uk-button uk-button-secondary" target="_blank">Documentation</a>
+                    <a href="db-setup.php" class="uk-button uk-button-primary">Begin Setup</a>
+                  </center>
+                </div>
+              </div>
             </div>
+            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+  </body>
 </html>
