e5bc723418d3daddd14aa73bdf4a27405574bee1
GabeSystems <straxxmusicteam@gmail.com>
2019-08-24 17:59:40 -0400

    setup complete page

---
+++ setup/setup-complete.php
@@ -0,0 +1,86 @@
+<?php
+/*
+
+      _____  _       _          ____  _ _ _ _
+     |  __ \| |     | |        |  _ \(_) | (_)
+     | |__) | |_   _| |_ ___   | |_) |_| | |_ _ __   __ _
+     |  ___/| | | | | __/ _ \  |  _ <| | | | | '_ \ / _` |
+     | |    | | |_| | || (_) | | |_) | | | | | | | | (_| |
+     |_|    |_|\__,_|\__\___/  |____/|_|_|_|_|_| |_|\__, |
+                                                     __/ |
+                                                    |___/
+
+    Copyright (c) 2019 CrypticNode LLC <pluto@crypticnode.host>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+*/
+?>
+
+<html>
+  <head>
+    <title>Pluto Billing - Software Setup</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/notyf.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+  </head>
+  <body>
+    <div uk-sticky="media: 960" class="uk-navbar-container tm-navbar-container uk-sticky uk-active" style="position: fixed; top: 0px; width: 1903px;">
+      <div class="uk-container uk-container-expand">
+        <nav uk-navbar>
+          <div class="uk-navbar-left">
+            <a href="" class="uk-navbar-item uk-logo">
+            Pluto Billing - Software Setup
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="content-background">
+      <div class="uk-section-large">
+        <div class="uk-container uk-container-large">
+          <div uk-grid class="uk-child-width-1-1@s uk-child-width-2-3@l">
+            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
+            <div class="uk-width-1-1@s uk-width-3-5@l uk-width-1-3@xl">
+              <div class="uk-card uk-card-default">
+                <div class="uk-card-header">
+                  <span class="uk-text-bold">Step Four:</span> Remove Setup Folder
+                </div>
+                <div class="uk-card-body">
+                  <center>
+                    <p>Congratulations you've finished the setup. <span class="uk-text-bold">You must remove the <code>/setup</code> folder to access the rest of pluto!</span></p>
+                    <a href="https://pluto.crypticnode.host/docs" class="uk-button uk-button-secondary" target="_blank">Documentation</a>
+                    <a href="../admin/" class="uk-button uk-button-primary">Dashboard</a>
+                  </center>
+                </div>
+              </div>
+            </div>
+            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
