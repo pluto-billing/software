8719a6ffb0ea9d2ec75492823dd4c59702b06fd0
GabeSystems <straxxmusicteam@gmail.com>
2019-08-26 00:35:07 -0400

    header and footer includes

---
+++ admin/resources/includes/Footer.php
@@ -0,0 +1,48 @@
+<?php
+/*
+
+      _____  _       _          ____  _ _ _ _
+     |  __ \| |     | |        |  _ \(_) | (_)
+     | |__) | |_   _| |_ ___   | |_) |_| | |_ _ __   __ _
+     |  ___/| | | | | __/ _ \  |  _ <| | | | | '_ \ / _` |
+     | |    | | |_| | || (_) | | |_) | | | | | | | | (_| |
+     |_|    |_|\__,_|\__\___/  |____/|_|_|_|_|_| |_|\__, |
+                                                     __/ |
+                                                    |___/
+
+    Copyright (c) 2019 CrypticNode LLC <pluto@crypticnode.host>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+*/
+?>
+
+<footer>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.min.js" integrity="sha256-UGwvyUFH6Qqn0PSyQVw4q3vIX0wV1miKTracNJzAWPc=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.transit/0.9.12/jquery.transit.min.js" integrity="sha256-rqEXy4JTnKZom8mLVQpvni3QHbynfjPmPxQVsPZgmJY=" crossorigin="anonymous"></script>
+  <script src="./public/javascript/notyf.min.js"></script>
+  <script src="./public/javascript/script.js"></script>
+  <script src="./public/javascript/status.js"></script>
+  <script src="./public/javascript/charts.js"></script>
+  <script src="./public/javascript/notification.js"></script>
+  <script src="./vendor/uikit3/uikit.min.js"></script>
+  <script src="./vendor/uikit3/uikit-icons.min.js"></script>
+</footer>
---
+++ admin/resources/includes/Header.php
@@ -0,0 +1,49 @@
+<?php
+/*
+
+      _____  _       _          ____  _ _ _ _
+     |  __ \| |     | |        |  _ \(_) | (_)
+     | |__) | |_   _| |_ ___   | |_) |_| | |_ _ __   __ _
+     |  ___/| | | | | __/ _ \  |  _ <| | | | | '_ \ / _` |
+     | |    | | |_| | || (_) | | |_) | | | | | | | | (_| |
+     |_|    |_|\__,_|\__\___/  |____/|_|_|_|_|_| |_|\__, |
+                                                     __/ |
+                                                    |___/
+
+    Copyright (c) 2019 CrypticNode LLC <pluto@crypticnode.host>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Pluto Admin Dashboard - <?php echo $page_title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./vendor/uikit3/uikit.min.css" />
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="./public/css/pluto-admin.css" />
+    <link rel="stylesheet" href="./public/css/notyf.min.css" />
+  </head>
+  <body>
